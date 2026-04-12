52b6f0610beae4f36ff0f5550b8dd9a0077d75c2
Oscar Casas <oscaralexander2626@gmail.com>
2026-04-12 11:55:45 -0500

    feat: enable React compiler and configure API proxy rewrites in next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://44.198.175.55:4000/:path*', // Tu IP de AWS
+      },
+    ];
+  },
 };
 
 export default nextConfig;
